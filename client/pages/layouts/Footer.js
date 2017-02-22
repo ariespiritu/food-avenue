f2450c2878dcbd5382d7f0c192160f205cc5dfa0
Oh No <Oh No>
2017-02-22 21:19:33 +0800

    Updated navigation

--- client/pages/layouts/Footer.js
+++ client/pages/layouts/Footer.js
@@ -4,7 +4,7 @@ const Footer = React.createClass({
   render() {
     return (
       <div className="my-footer">
-        <h5 className="text-center"> Footer</h5>
+        <h5 className="text-center"> About Us, haha teneeennn. na set up ko na hal. gagawan ko bukas :D yiiee </h5>
       </div>
     );
   }
--- client/pages/layouts/Nav.js
+++ client/pages/layouts/Nav.js
@@ -3,9 +3,36 @@ import React from 'react'
 const Nav = React.createClass({
   render() {
     return (
-      <nav className="navbar navbar-default navbar-fixed-top my-nav">
-        <div className="container">
-          <h5 className="text-center"> Navigation bar </h5>
+      <nav className="navbar navbar-toggleable-md navbar-light fixed-top">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">
+          <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="Food Avenue Logo" />
+        </a>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+          </ul>
+          <div className="form-inline my-2 my-lg-0">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Buffet</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Menu</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About Us</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
